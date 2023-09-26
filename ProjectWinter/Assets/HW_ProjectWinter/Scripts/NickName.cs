b303d910b845fc926692e6a2d78e67b9104caf4d
정현우\jhw50 <jhw504166@gmail.com>
2023-09-26 09:45:25 +0900

    Init

--- ProjectWinter/Assets/HW_ProjectWinter/Scripts/NickName.cs
+++ ProjectWinter/Assets/HW_ProjectWinter/Scripts/NickName.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class NickName : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Camera mainCamera; // 메인 카메라에 대한 참조
+
+    private void Start()
     {
-        
+        mainCamera = Camera.main; // 메인 카메라를 찾아서 저장
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // 항상 카메라 방향으로 회전
+        if (mainCamera != null)
+        {
+            // UI를 항상 카메라를 향하도록 회전
+            transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
+                mainCamera.transform.rotation * Vector3.up);
+        }
     }
 }
--- ProjectWinter/Assets/SG_ProjectWinter/Scripts/ItemScripts/SG_Item.cs
+++ ProjectWinter/Assets/SG_ProjectWinter/Scripts/ItemScripts/SG_Item.cs
@@ -18,8 +18,6 @@ public class SG_Item : ScriptableObject
 
     public GameObject handPrefab; // 플레이어손에 쥐어줄 프리팹
 
-    public GameObject handPrefab; // 플레이어손에 쥐어줄 프리팹
-
     public string weaponType; //무기 유형
 
     public int itemDamage;  // 아이템의 데미지
